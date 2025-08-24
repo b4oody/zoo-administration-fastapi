521919b6b9fb6b2e619a564bb81d01fed2cba8d3
VR <vladrymarchuk@gmail.com>
2025-08-25 01:11:56 +0300

    init project, add database settings, engine

---
+++ main.py
@@ -0,0 +1,26 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5555, reload=True)
